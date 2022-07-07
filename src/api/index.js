7a522634064caf795f5cd02bf40795f807f0ca3d
Simu <kangtaeam@gmail.com>
2022-07-07 11:10:26 -0400

    api and login

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,22 @@
-import axios from 'axios'
-
 const BASE = 'https://strangers-things.herokuapp.com/api'
 const COHORT = '2206-FTB-ET-WEB-FT-A'
 
+export async function loginUser(username, password) {
+    const response = await fetch(`${BASE}${COHORT}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: {
+        username: username,
+        password: password,
+        },
+      }),
+    }) .then(response => response.json())
+    .then(result => {
+      console.log(result);
+    })
+    .catch(console.error);
+
+}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import { loginUser } from '../api'
 
 const Login = () => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    let localToken = '';
+
+    async function handleSubmit(event) {
+        event.preventDefault()
+        const loginSuccess = await loginUser(username, password)
+    }
+    return <div>
+        
+        <h2>Login</h2>
+        
+        <input 
+        name = 'username'
+        type = 'text'
+        value = {username}
+        onChange = {(event) => {
+            setUsername(event.target.value)
+        }}
+        />
+        <input
+        name = 'password'
+        type = 'text'
+        value = {password}
+        onChange = {(event) => {
+            setPassword(event.target.value)
+        }}
+        />
+        <button id='submit'>Login</button>
+
+    </div>
 }
 
 export default Login
