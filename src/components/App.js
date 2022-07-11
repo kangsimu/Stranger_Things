c98771836f4ced92c9cbbe5a3564cd3ddf5a9638
Simu <kangtaeam@gmail.com>
2022-07-11 11:35:19 -0400

    fixed lol

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import {Routes, Route} from "react-router-dom"
 import {Posts, Profile, Login, Register, Header} from "./"
 
